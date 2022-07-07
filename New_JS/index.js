8d0680193e5704cc47aa31d1baee82ec30d8dacc
kunal232i <kingawale23@gmail.com>
2022-07-07 13:31:07 +0000

    New

--- New_JS/index.js
+++ New_JS/index.js
@@ -1,6 +1,20 @@
 console.log("Hello World");
 
+//number
 var num1 = 34;
 var num2 = 66;
 
 console.log(num1 + num2);
+
+//strings
+var str1 = "New string";
+var str2 = "New string ++";
+
+//objects
+var marks = {
+  kunal: 70,
+  Om: 80,
+  amey: 89,
+};
+
+console.log(str2 + " " + marks.kunal + " " + num1);
