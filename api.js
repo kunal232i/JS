1ba3ec914e409b55d0d27643b8eca77dc291dd7f
kunal232i <kingawale23@gmail.com>
2022-07-07 14:46:06 +0000

    changes

--- api.js
+++
@@ -1,8 +0,0 @@
-fetch("https://cat-fact.herokuapp.com/facts").then(response=>response.json())
-.then(obj=>{
-    console.log(obj)
-    for(let i=0; i<obj.length; i++){
-        console.log(obj[i].text);
-    }
-})
-
