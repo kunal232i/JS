58f2243624d274c9370d8341ae3be3fdb7ad6ca7
kunal232i <kingawale23@gmail.com>
2022-07-07 14:44:15 +0000

    apps add

---
+++ New_JS/WeatherApp.js
@@ -0,0 +1,9 @@
+//if rain 👉 'Grab your umbrella ☔'
+//else 👉 'wear your sunglasses 🕶️😎'
+
+let weather = prompt("Is It Rainning outside");
+if (weather == "yes") {
+  console.log("Grab your umbrella ☔");
+} else {
+  console.log("wear your sunglasses 🕶️😎");
+}
--- New_JS/index.js
+++ New_JS/index.js
@@ -1,20 +1,37 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 //number
-var num1 = 34;
-var num2 = 66;
+let num1 = 34;
+let num2 = 66;
 
 console.log(num1 + num2);
 
 //strings
-var str1 = "New string";
-var str2 = "New string ++";
+let str2 = "New string ++";
 
-//objects
-var marks = {
-  kunal: 70,
-  Om: 80,
-  amey: 89,
-};
+// //Operators
+// food = 20;
+// tip = 0.5;
+// tipAmount = food + tip;
+// console.log(tipAmount);
 
-console.log(str2 + " " + marks.kunal + " " + num1);
+// //objects
+// marks = {
+//   kunal: 70,
+//   Om: 80,
+//   amey: 89,
+// };
+
+// console.log(str2 + " " + marks.kunal + " " + num1);
+
+//User Input
+// fruit = prompt("What is your fav Fruit");
+// console.log(fruit);
+
+// let Food = Number(prompt("How much was the food ?"));
+// let tip = Number(prompt("Tip")) / 100;
+// let tipAmount = Food * tip;
+// console.log(" Tip Amount", tipAmount);
+
+// Total = Food + tipAmount;
+// console.log("Total", Total);
