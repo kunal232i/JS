096724b9c6e9d8bcca11d56b779708c89aaeb1bb
kunal232i <kingawale23@gmail.com>
2022-07-07 17:40:23 +0000

    function

--- New_JS/Calculator.js
+++ New_JS/Calculator.js
@@ -1,5 +1,10 @@
 function sayMyName(name) {
   console.log(name);
 }
-
 sayMyName("Kunal");
+
+function greeting(name) {
+  greet = `Hi ${name}, nice to meet you!!`;
+  console.log();
+}
+greeting("kunal");
