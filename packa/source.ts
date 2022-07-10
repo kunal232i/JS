21424cf1e3c620f276c45f551f10cddc18c34271
kunal232i <kingawale23@gmail.com>
2022-07-10 12:24:45 +0000

    delete

--- packa/source.ts
+++
@@ -1,3 +0,0 @@
-import Person, { car } from "./Person";
-export let kunal = new Person("Kunal", "diploma", 19);
-console.log(car);
--- typescript/app.ts
+++
@@ -1,7 +0,0 @@
-// let count : number = 0;
-// let names : string = "rer";
-// let list : string[] = ["ere","erer","Erer"]
-// let ok : boolean = true
-// interface Person {
-
-// }
