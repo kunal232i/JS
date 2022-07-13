8e3b0a3fdae0c8003a851cba491f2e3f5bb8d6e0
kunal232i <kingawale23@gmail.com>
2022-07-13 12:46:56 +0000

    DOM

--- New_JS/DOM.js
+++ New_JS/DOM.js
@@ -1,9 +1,34 @@
-let titleDIV = document.getElementById("title");
+// let titleDIV = document.getElementById("title");
 
-console.log("before: ", titleDIV.innerText);
+// console.log("before: ", titleDIV.innerText);
 
-let msg = "Goodbyee !!";
-titleDIV.innerText = msg;
-console.log("after: ", titleDIV.innerText);
+// let msg = "Goodbyee !!";
+// titleDIV.innerText = msg;
+// console.log("after: ", titleDIV.innerText);
 
-titleDIV.innerHTML = `<p>${msg}</p>`;
+// titleDIV.innerHTML = `<p>${msg}</p>`;
+
+// titleDIV.style.backgroundColor = "blue";
+
+let redDiv = document.getElementById("red");
+let yellowDiv = document.getElementById("yellow");
+let greenDiv = document.getElementById("green");
+
+// redDiv.onclick = () => console.log("red");
+// yellowDiv.onclick = () => console.log("yellow");
+// greenDiv.onclick = () => console.log("green");
+
+const squares = document.querySelectorAll(".colorSquare");
+// console.log(squares);
+// console.log(squares[0].value);
+// console.log(squares[1].value);
+// console.log(squares[2].value);
+
+//for Each
+const timesClicked = { red: 0, yellow: 0, green: 0 };
+squares.forEach((square) => {
+  square.onclick = () => console.log(square.value);
+  timesClicked[square.value] += 1;
+  square.innerText = timesClicked[square.value];
+  //   square.innerText = timesClicked[square.value];
+});
