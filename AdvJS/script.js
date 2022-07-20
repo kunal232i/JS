72413944f7d407d4e3048d3687e1c5e5fe94186e
kunal232i <kingawale23@gmail.com>
2022-07-20 18:04:56 +0000

    async await

--- AdvJS/script.js
+++ AdvJS/script.js
@@ -7,29 +7,44 @@
 // console.log("still speaking");
 
 //promises
-// const Promise1 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     isReeady = [true, false][Math.floor(Math.random() * 2)]; //randomly chooosing true or false
-//     isReeady // condition
-//       ? resolve("soup is ready") // runs if true
-//       : reject("No soup today"); // runs if falses
-//     // ternary operators
-//   }, 2000);
-// });
+const Promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    isReeady = [true, false][Math.floor(Math.random() * 2)]; //randomly chooosing true or false
+    isReeady // condition
+      ? resolve("soup is ready") // runs if true
+      : reject("No soup today"); // runs if falses
+    // ternary operators
+  }, 1000);
+});
 
 // console.log(
 //   Promise1.then((success) => console.log({ success })) //printing the resolve value
 //     .catch((error) => console.log({ error })) //catching the rejected value
 // );
 
-console.log(
-  "fetch:",
-  fetch("https://dog.ceo/api/breeds/image/random") // promise
-    .then((response) => console.log("response: ", response.json())) //promise
-    .then((data) => console.log(data))
-);
+const getSoup = async () => {
+  try {
+    const soup = await Promise1;
+    console.log(soup);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getSoup();
+
+// const url = "https://dog.ceo/api/breeds/image/random";
+// fetch(url) // promise
+//   .then((response) => response.json()) //promise
+//   .then((data) => console.log(data));
 
 //new syntax ES6 for fetching API
 //Async await
 
-const
+// const getDog = async () => {
+//   const url = "https://dog.ceo/api/breeds/image/random";
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   console.log(data);
+// };
+
+// getDog();
