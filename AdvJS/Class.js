b4bf919149681b78b607730d388d7e580e754a55
kunal232i <kingawale23@gmail.com>
2022-07-22 19:17:01 +0000

    class

--- AdvJS/Class.js
+++ AdvJS/Class.js
@@ -1,63 +1,78 @@
-class Car {
-  constructor(name, color, topSpeed) {
-    this.name = name;
-    this.color = color;
-    this.topSpeed = topSpeed;
-    this.currSpeed = 0;
-  }
+// class Car {
+//   constructor(name, color, topSpeed) {
+//     this.name = name;
+//     this.color = color;
+//     this.topSpeed = topSpeed;
+//     this.currSpeed = 0;
+//   }
 
-  ZeroToHudread() {
-    setTimeout(() => {
-      console.log("phew, that was fast!!");
-      this.currSpeed = 100;
-      console.log(this.currSpeed);
-    });
-  }
-  drive() {
-    this.currSpeed += 10;
-    console.log(`driving at ${this.currSpeed} kmp`);
-  }
+//   getCurrentSpeed() {
+//     return currSpeed;
+//   }
 
-  brake() {
-    this.currSpeed -= 10;
-    console.log(`braking!!,current speed is ${this.currSpeed} kmp`);
-  }
-  stop() {
-    this.currSpeed = 0;
-    console.log(`coming to stop ${this.currSpeed} kmp`);
-  }
-}
+//   ZeroToHudread() {
+//     setTimeout(() => {
+//       this.currSpeed = 100;
+//       console.log(`phew, that was fast!! ${this.currSpeed} kmp`);
+//     });
+//   }
+//   drive() {
+//     this.currSpeed += 10;
+//     console.log(`driving at ${this.currSpeed} kmp`);
+//   }
 
-// const ferrari = new Car("Ferrari", "red", 450);
-// console.log(ferrari);
+//   brake() {
+//     this.currSpeed -= 10;
+//     console.log(`braking!!,current speed is ${this.currSpeed} kmp`);
+//   }
+//   stop() {
+//     this.currSpeed = 0;
+//     console.log(`coming to stop ${this.currSpeed} kmp`);
+//   }
+// }
+
+// // const ferrari = new Car("Ferrari", "red", 450);
+// // console.log(ferrari);
+// // // ferrari.drive();
+// // // ferrari.bratopSpeed;
+// // // console.log(ferrari.currSpeed);
+// // // ferrari.drive();
+// // // console.log(ferrari.currSpeed);
+// // // ferrari.ZeroToHudread();
+// // // console.log(ferrari.currSpeed);
+
 // // ferrari.drive();
-// // ferrari.bratopSpeed;
-// // console.log(ferrari.currSpeed);
+// // ferrari.drive();
+// // ferrari.drive();
+// // ferrari.drive();
 // // ferrari.drive();
 // // console.log(ferrari.currSpeed);
-// // ferrari.ZeroToHudread();
+// // ferrari.brake();
 // // console.log(ferrari.currSpeed);
 
-// ferrari.drive();
-// ferrari.drive();
-// ferrari.drive();
-// ferrari.drive();
-// ferrari.drive();
-// console.log(ferrari.currSpeed);
-// ferrari.brake();
-// console.log(ferrari.currSpeed);
+// // ferrari.stop();
+// // drive();
 
-// ferrari.stop();
-// drive();
+// const porsche = new Car("porsche", "yellow", 350);
 
-const porsche = new Car("porsche", "yellow", 350);
+// console.log(porsche);
+// // porsche.drive();
+// // porsche.drive();
+// // porsche.drive();
+// // porsche.drive();
+// const nums = [1, 2, 3, 4, 5];
+// nums.forEach(() => porsche.drive());
+// porsche.ZeroToHudread();
 
-console.log(porsche);
-// porsche.drive();
-// porsche.drive();
-// porsche.drive();
-// porsche.drive();
-const nums = [1, 2, 3, 4, 5];
-nums.forEach(() => porsche.drive());
-porsche.brake();
-porsche.stop();
+Array.prototype.myPush = function (item) {
+  this[this.length] = item;
+  return this;
+};
+
+const fruits = ["🍎", "🍏", "🍌", "🍐"];
+console.log(fruits.myPush("🥝"));
+fruits.myPush("🍇");
+fruits.myPush("🍈");
+fruits.myPush("🍉");
+fruits.myPush("🍊");
+console.log(fruits);
